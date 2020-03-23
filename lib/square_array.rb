a1bb8a641b9a3a4806df37b86f8ecfc97b334941
Brandon Yoon <byoon5397@gmail.com>
2020-03-23 05:38:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,11 +3,10 @@ def square_array(array)
   i = 0
   while i <= array.length
     num = array[i]
-    sqNum = num * num
+    sqNum = num**2
     newArr << sqNum
     i += 1
   end
 
   return newArr
 end
-
